4a0de372ab3cf2ce5f62b1721f3e12e34611a84a
Charles Belot <charles.belot@enigma-school.com>
2026-03-31 13:15:20 +0200

    adding favorite calling and displaying it in cards

--- src/components/CardRecipe/CardRecipe.jsx
+++ src/components/CardRecipe/CardRecipe.jsx
@@ -1,13 +1,15 @@
-
 import React from 'react';
 import styles from './CardRecipe.module.scss';
 
-const CardRecipe = ({recipe}) => {
+const CardRecipe = ({recipe, isFavorite, onToggleFav}) => {
     return (
         <div className={styles.card}>
             <h2>{recipe.name}</h2>
             <p>{recipe.cuisine}</p>
             <img src={recipe.image} alt={recipe.name} />
+            <button onClick={() => onToggleFav(recipe.id)}>
+                {isFavorite ? '❤️' : '🤍'}
+            </button>
         </div>
     );
 };
