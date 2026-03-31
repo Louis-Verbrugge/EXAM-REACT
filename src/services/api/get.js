3ea208b128b91118f06968fa8b6baca76ef5ee87
Verbrugge <90406459+Louis-Verbrugge@users.noreply.github.com>
2026-03-31 11:16:49 +0200

    init function API

---
+++ src/services/api/get.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+export const getData = async (path, params = {}, config = {}) => {
+    const token = localStorage.getItem("user") ?? "";
+
+    try {
+        const response = await axios.get('https://dummyjson.com/'+ path, {
+            params: params,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            },
+            ...config
+        });
+
+        return response.data ?? null;
+
+    } catch (error) {
+        console.error("Error fetching post data:", error);
+        
+        return null;
+    }
+}
