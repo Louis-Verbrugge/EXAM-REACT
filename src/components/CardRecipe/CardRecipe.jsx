2bf84451ef5ba65656ebed9e0dd42b8a67f31a3f
Verbrugge <90406459+Louis-Verbrugge@users.noreply.github.com>
2026-03-31 11:11:52 +0200

    init CardRecipe and components

---
+++ src/components/CardRecipe/CardRecipe.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import styles from './CardRecipe.module.scss';
+
+const CardRecipe = ({}) => {
+    return (
+        <div>
+            Component content
+        </div>
+    );
+};
+
+export default CardRecipe;
